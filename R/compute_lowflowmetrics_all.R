b9d37f8c1a6dc46bb58a84ceb98992585d502960
naomitague <ctague@bren.ucsb.edu>
2022-05-31 09:13:40 -0700

    make sure summarize is used correctly

--- R/compute_lowflowmetrics_all.R
+++ R/compute_lowflowmetrics_all.R
@@ -19,7 +19,7 @@ compute_lowflowmetrics_all = function(m,o, month, day, year,wy, low_flow_months=
   flow = cbind.data.frame(m,o, month, day, year,wy)
   # first lets get minimum yearly values
 
-  tmp = flow %>% group_by(wy) %>% summarize(mino=min(o), minm=min(m))
+  tmp = flow %>% group_by(wy) %>% dplyr::summarize(mino=min(o), minm=min(m))
 
   annual_min_err = mean(tmp$minm-tmp$mino)
 
@@ -29,7 +29,7 @@ compute_lowflowmetrics_all = function(m,o, month, day, year,wy, low_flow_months=
   { max_err_annual_min = 0.5*mean(tmp$mino)}
 
   # now lets get monthly values
-  tmp = flow %>% group_by(month, year) %>% summarize(model=sum(m), obs=sum(o))
+  tmp = flow %>% group_by(month, year) %>% dplyr::summarize(model=sum(m), obs=sum(o))
   # now extract august
   low = subset(tmp, month %in% low_flow_months)
   low_month_err = mean(low$model-low$obs)
